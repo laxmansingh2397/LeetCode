c67c6cc8c4875a3bca935cb264a550de0b9dfca6
Laxman Singh <singhlaxman2397@gmail.com>
2025-08-06 10:43:01 +0530

    Solved problem add_binary

---
+++ day-2/add_binary.py
@@ -0,0 +1,32 @@
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+
+        answer = []
+        carry = 0
+        i,j = len(a) - 1, len(b) - 1
+
+        while i >= 0 or j >= 0 or carry:
+            total = carry
+            if i >= 0:
+                total += int(a[i])
+                i -= 1
+            if j >= 0:
+                total += int(b[j])
+                j -= 1
+            answer.append(str(total % 2))
+            carry = total // 2
+
+        return "".join(reversed(answer))
+    
+if __name__ == "__main__":
+
+    a = input("Enter first binary number: ")   
+    b = input("Enter second binary number: ")
+
+    sol = Solution()
+
+    result = sol.addBinary(a,b)
+
+    print(result)
+    
